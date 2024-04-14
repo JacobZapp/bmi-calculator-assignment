0657754ace57f81e3412302a407f7241baec8bb2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-04-14 18:38:16 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import BodyMassIndex from "./components/BodyMassIndex.vue";
+
+import { ref } from 'vue'
+
+// const bmi = weight / (height * height)
+
+function calculatedBMI(bmi) {
+
+  calculatedBMI.value = `Your BMI is: ${bmi}`
+}
+
+</script>
+
+<template>
+
+    <h1>Body Mass Index Calculator</h1>
+
+
+  <BodyMassIndex
+  v-on:stats="calculatedBMI"></BodyMassIndex>
+
+  <p> {{ calculatedBMI }}</p>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/BodyMassIndex.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+// const emit = defineEmits([
+//   height, weight
+// ])
+
+
+
+const height = ref('')
+const weight = ref('')
+
+const stats = [height, weight]
+
+
+</script>
+
+<template>
+  <div id="BMI">
+    <form>
+    <h2> Enter your Height and Weight</h2>
+
+      <label> Height in Meters: </label>
+      <input v-model="height" type="text">
+
+    <br>
+
+      <label> Weight in Kilograms: </label>
+      <input v-model="weight" type="text">
+
+    <br>
+
+    <button v-on:click="defineEmits([stats-entered])">Calculate</button>
+    </form>
+
+
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
